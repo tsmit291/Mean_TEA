733b6261137cad3f97c0e4d9340a9a45ffb986f4
= <tsmit291@icloud.com>
2016-02-27 15:49:50 -0700

    partials complete

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 // this is where your routes go //
 var app = angular.module('meanTEA', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: './partials/home.html',
+    controller: 'mainController'
+  })
+  .when('/about', {
+    templateUrl: './partials/about.html',
+    controller: 'aboutController'
+  })
+  .when('/contact', {
+    templateUrl: './partials/contact.html',
+    controller: 'contactController'
+  })
+});
