2403072cd6453d8517c6bf445384944e9a07c7a4
= <tsmit291@icloud.com>
2016-02-28 17:20:10 -0700

    teas printing to the page

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -4,16 +4,8 @@ app.controller('mainController', ['$scope', 'teaService', function($scope, teaSe
   var promise = teaService.getTea();
 
   promise.then(function(collection){
-// whatever results are from promise (once it is relolved) this gets set equal to teas. Then I call teas.
-    $scope.teas = collection.data;
-    console.log($scope.teas);
+// whatever results are from promise (once it is resolved) this gets set equal to teas. Then I call teas.
+    $scope.teaCollection = collection.data;
+    console.log($scope.teaCollection);
   });
 }]);
-
-//
-//
-// var promise = Tea.getall();
-//   promise.then(function(data) {
-//   $scope.teas = data.data;
-//   console.log('5 ********** ', $scope.teas);
-// })
